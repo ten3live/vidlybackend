7f72eff48f4cab86c3d175898f156f9f99d23492
ten3live <ten3live@gmail.com>
2021-03-30 09:18:06 +0500

    make it better

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
